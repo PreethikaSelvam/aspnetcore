84e15167176bec1bafad4c2e08fcc26cf2b46680
PreethikaSelvam <preethika.selvam@syncfusion.com>
2026-09-24 12:19:36 +0530

    Updated the feature scope.

--- src/Components/Samples/BlazorWebAppGlobal/wwwroot/theme.js
+++ src/Components/Samples/BlazorWebAppGlobal/wwwroot/theme.js
@@ -1,16 +1,19 @@
-const colorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const colorScheme = window.matchMedia?.('(prefers-color-scheme: dark)');
 
 function updateTheme() {
-  const theme = colorScheme.matches ? 'dark' : 'light';
+  const theme = colorScheme?.matches ? 'dark' : 'light';
   if (document.documentElement.getAttribute('data-bs-theme') !== theme) {
     document.documentElement.setAttribute('data-bs-theme', theme);
   }
 }
 
-new MutationObserver(updateTheme).observe(document.documentElement, {
-  attributes: true,
-  attributeFilter: ['data-bs-theme'],
-});
-
 updateTheme();
-colorScheme.addEventListener('change', updateTheme);
+
+colorScheme?.addEventListener?.('change', updateTheme);
+
+if (window.MutationObserver) {
+  new MutationObserver(updateTheme).observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ['data-bs-theme'],
+  });
+}
